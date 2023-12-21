8f78c77283c920d7c5a8b1283f119a5064710e89
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-12-21 14:44:17 +0500

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+export default function App() {
+	return (
+		<>
+			<Header />
+			<main className="main">
+				
+			</main>
+			<Footer />
+		</>
+	);
+}
